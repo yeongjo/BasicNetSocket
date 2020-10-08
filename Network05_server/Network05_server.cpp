c0e3fcb08de187adaf51b284a4d2e3f8e28b90e6
sc2466 <songchung2466@gmail.com>
2020-10-08 18:32:14 +0900

    Revert "network05 완료"
    This reverts commit 6bd14ac9d3670fc8ca7e56f540524cdad120135c.

--- Network05_server/Network05_server.cpp
+++ Network05_server/Network05_server.cpp
@@ -1,41 +1,126 @@
-﻿#include "../commonSocket.h"
-int DataReceive(SOCKET s, char* buf, int len, int flags, bool isPrint = false) {
-	char *ptr = buf;
+﻿#include <stdlib.h>
+#include <stdio.h>
+#include <WinSock2.h>
+
+#define BUFSIZE 4096
+#pragma warning(disable:4996)
+#pragma comment(lib, "Ws2_32.lib")
+
+// 데이터 수신 함수
+int dataReceive(SOCKET s, char* buf, int len, int flags) {
+
+	// 통신에 필요한 변수 선언
 	int received;
-	for (int left = len; 0 < left; left -= received, ptr += received) {
+	char *ptr = buf;
+	int left = len;
+
+	// 남아있는 데이터가 있는 경우 반복적으로 시행
+	while (left > 0) {
 		received = recv(s, ptr, left, flags);
-		if (received == SOCKET_ERROR)	return SOCKET_ERROR;
-		if (received == 0)				return len - left;
-		if (isPrint) PrintPercent(ptr - buf, len, received);
+
+		// 더이상 데이터를 받아오지 못하는 경우
+		if (received == 0)
+			break;
+
+		// 받아온 데이터가 존재하는 경우
+		left -= received;
+		ptr += received;
+
+		printf("%d 퍼센트\n", (int)((ptr-buf) / (float)len * 100));
 	}
-	return -2;
+
+	// 더 받을 수 있는 데이터 길이를 반환(데이터를 한 번이라도 받았으면 0이 아닌 수를 반환하게 됨)
+	return (len - left);
 }
 
-void main(int argc, char *argv[]) {
-	SOCKET listenSock = CreateSocket();
+// 메인 함수
+int main(int argc, char *argv[]) {
+
+	// 데이터 크기를 담을 변수
+	int retval = 1;
+
+	// 윈속 객체 선언 및 초기화
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
+
+	// 연결용 소켓을 생성
+	SOCKET listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+
+	// 바인딩 처리 변수 초기화
+	SOCKADDR_IN servaddr;
+	ZeroMemory(&servaddr, sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_port = htons(9000);
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	// 바인딩
+	bind(listen_sock, (SOCKADDR *)&servaddr, sizeof(servaddr));
+
+	// 연결 함수 시작
+	listen(listen_sock, SOMAXCONN);
+
+	// 데이터 통신에 사용할 변수
+	SOCKET client_sock;
+	SOCKADDR_IN clientaddr;
+	int addrlen;
+	char* buf;
+
+	// 반복적으로 클라이언트로부터 소켓 요청을 받음
 	while (1) {
-		SOCKET clientSock = AccpetClient(listenSock);
-		char fileName[256]; ZeroMemory(fileName, 256);
-		int totalBytes;
-		DataReceive(clientSock, fileName, 256, 0);
-		DataReceive(clientSock, (char *)&totalBytes, sizeof(totalBytes), 0);
 
-		printf("받을 파일 이름 : %s\n", fileName);
-		printf("받을 파일 크기 : %d 바이트\n", totalBytes);
+		// 클라이언트 접속을 받음
+		addrlen = sizeof(clientaddr);
+		client_sock = accept(listen_sock, (SOCKADDR *)&clientaddr, &addrlen);
 
-		FILE *fp = fopen(fileName, "w+b");	// 빈파일만듬
-		char* buf = new char[totalBytes];	// 버퍼할당
-		int remain = DataReceive(clientSock, buf, totalBytes, 0, true);
-		if (remain == SOCKET_ERROR) {
-			printf("\n파일읽는데 오류\n");
-			continue;
+		// 접속한 클라이언트 정보 출력
+		printf("클라이언트 접속 : IP = %s, Port = %d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
+
+		// 파일 이름 받기
+		char filename[256];
+		ZeroMemory(filename, 256);
+		dataReceive(client_sock, filename, 256, 0);
+		printf("받을 파일 이름 : %s\n", filename);
+
+		// 파일 크기 받기
+		int totalbytes, remainBytes;
+		dataReceive(client_sock, (char *)&totalbytes, sizeof(totalbytes), 0);
+		buf = new char[totalbytes];
+		printf("받을 파일 크기 : %d 바이트\n", totalbytes);
+
+		// 파일 열기
+		FILE *fp = fopen(filename, "wb");
+
+		// 파일 데이터 받기
+		int numtotal = 0;
+		while (retval != 0) {
+			remainBytes = totalbytes - BUFSIZE;
+			int bufSize = BUFSIZE;
+			if (remainBytes < 0)
+				bufSize = remainBytes;
+			retval = dataReceive(client_sock, buf, bufSize, 0);
+
+			fwrite(buf, 1, retval, fp);
+
+			// 받은 데이터 크기만큼 변수에 더해줌
+			numtotal += retval;
 		}
-		fwrite(buf, 1, remain, fp);
-		printf("\n파일을 성공적으로 받았습니다.\n");
 
 		fclose(fp);
-		closesocket(clientSock);
+
+		// 전송 결과
+		if (numtotal == totalbytes)
+			printf("파일을 성공적으로 받았습니다.\n");
+		else
+			printf("파일을 제대로 받지 못했습니다.\n");
+
+		// 해당 클라이언트 소켓을 폐기
+		closesocket(client_sock);
 	}
-	closesocket(listenSock);
+
+	// 서버 소켓을 폐기
+	closesocket(listen_sock);
+
+	// 윈속 종료
 	WSACleanup();
+	return 0;
 }
--- commonSocket.h
+++ commonSocket.h
@@ -1,5 +1,4 @@
 #define _WINSOCK_DEPRECATED_NO_WARNINGS // 최신 VC++ 컴파일 시 경고 방지
-#define _CRT_SECURE_NO_WARNINGS
 #pragma comment(lib, "ws2_32")
 #include <winsock2.h>
 #include <ws2tcpip.h>
@@ -9,7 +8,7 @@
 #include <cctype>
 
 #define SERVERPORT 9000
-#define BUFSIZE    4096
+#define BUFSIZE    512
 
 using namespace std;
 
@@ -36,38 +35,4 @@ void err_display(const char *msg) {
 		(LPTSTR)&lpMsgBuf, 0, NULL);
 	printf("[%s] %s", msg, (char *)lpMsgBuf);
 	LocalFree(lpMsgBuf);
-}
-
-// 첨파라미터 bind, connect 사용
-// addr: 주소 inet_addr("127.0.0.1")
-SOCKET CreateSocket(ULONG addr = htonl(INADDR_ANY)) {
-	WSADATA wsa;
-	WSAStartup(MAKEWORD(2, 2), &wsa);
-	SOCKADDR_IN servaddr;
-	ZeroMemory(&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(9000);
-	servaddr.sin_addr.s_addr = addr;
-
-	auto s = socket(AF_INET, SOCK_STREAM, 0);
-	if (addr == htonl(INADDR_ANY)) {
-		bind(s, (SOCKADDR *)&servaddr, sizeof(servaddr));
-		listen(s, SOMAXCONN);
-	} else {
-		connect(s, (SOCKADDR *)&servaddr, sizeof(servaddr));
-		printf("서버 접속 : IP = %s, Port = %d\n", inet_ntoa(servaddr.sin_addr), ntohs(servaddr.sin_port));
-	}
-
-	return s;
-}
-
-void PrintPercent(float current, float total, float bufSize) {
-	printf("\r%.1f 퍼센트 \t bufsize: %d", (current / (float)total * 100), bufSize);
-}
-
-SOCKET AccpetClient(SOCKET listen_sock) {
-	SOCKADDR_IN clientaddr; int addrlen = sizeof(clientaddr);
-	SOCKET client_sock = accept(listen_sock, (SOCKADDR *)&clientaddr, &addrlen);
-	printf("클라이언트 접속 : IP = %s, Port = %d\n", inet_ntoa(clientaddr.sin_addr), ntohs(clientaddr.sin_port));
-	return client_sock;
 }
